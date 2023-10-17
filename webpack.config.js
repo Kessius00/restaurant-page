a620c1965a862247bdc571be71bb0509dad30688
kessius00 <kes.olling00@gmail.com>
2023-10-17 02:42:59 +0200

    Add loaders, cache busting and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devtool: false,
+  entry: './src/index.js',
+  output: {
+    filename: 'main.[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: ["style-loader", "css-loader"]
       }
     ]
   },
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
 };
