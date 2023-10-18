9d634f318bf2b3f53c477cff03c2cd1b27e1afa8
kessius00 <kes.olling00@gmail.com>
2023-10-18 03:17:06 +0200

    Add html-loader and discover file-loader is obsolete in webpack 5

--- webpack.common.js
+++ webpack.common.js
@@ -3,12 +3,26 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    clean: true
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"]
-      }
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "img/[name].[hash].[ext]",
+        },
+      },
     ]
   },
   plugins: [new HtmlWebpackPlugin({
